35aa2fc5f8d3be5d9fd14655830a16b6a8cf7d76
Monique Alves Duarte <moniquead95@gmail.com>
2025-12-06 09:10:44 -0300

    fix: update next.js and security audit fix

--- src/components/site/Equipe.tsx
+++ src/components/site/Equipe.tsx
@@ -5,26 +5,26 @@ import Image from "next/image";
 type MembroEquipe = {
   name: string;
   role: string;
+  crmv: string;
   imageSrc: string;
   alt: string;
   unit: "Imbituba" | "Laguna";
 };
 
 const equipe: MembroEquipe[] = [
-  { name: "Dr. Douglas Vicentin", role: "Sócio Fundador, Diretor Geral | CRMV-SC 5003", imageSrc: "/equipe-Douglas.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Laguna"},
-  { name: "Dra. Ana Lívia Vicentin", role: "Sócia Fundadora, Anestesiologia e Medicina Felina | CRMV-SC 04952", imageSrc: "/equipe-1.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Laguna"},
-  { name: "Dr. Guilherme Pereira", role: "Anestesiologia, Responsável Técnico e Coordenador de equipe | CRMV-SC 11054", imageSrc: "/equipe-Gui.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Imbituba"},
-  { name: "Dra. Andressa Spengler", role: "Cirurgia de Tecidos Moles e Oncologia | CRMV-SC 14192", imageSrc: "/equipe-Andressa.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Imbituba"},
-  { name: "Dr. Ramon Santiago", role: "Clínica Geral, Medicina Felina | CRMV-SC 12456 ", imageSrc: "/equipe-Ramon.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Imbituba" },
-  { name: "Dra. Marina Noronha", role: "Clínica Geral | CRMV-SC 14205", imageSrc: "/equipe-Mari.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Imbituba"},
-  { name: "Dra. Bruna Oliveira", role: "Clínica Geral, Medicina Felina e Cirurgia de Tecidos Moles | CRMV-SC 9959", imageSrc: "/equipe-Bruna.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Imbituba"},
-  { name: "Dra. Caroline Tessmer", role: "Ultrassonografia - Diagnóstico por Imagem | CRMV-SC 12602", imageSrc: "/equipe-Carol.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Imbituba"}, 
-  { name: "Dra. Letícia de Oliveira", role: "Anestesiologia e Clínica Geral | CRMV-SC 12405", imageSrc: "/equipe-LeticiaOliveira.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Laguna"},
-  { name: "Dra. Letícia Laureano", role: "Clínica Geral | CRMV-SC 13580", imageSrc: "/equipe-LeticiaL.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Laguna"}, 
-  { name: "Dra. Bruna Micheleto", role: "Clínica Geral | CRMV-SC 15204", imageSrc: "/equipe-1.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Laguna"},
-  { name: "Dr. Vitor Anacleto", role: "Clínica Geral | CRMV-SC 14491", imageSrc: "/equipe-Vitor.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Laguna"},
-  { name: "Dra. Eduarda Sproesser", role: "Clínica Geral | CRMV-SC 15381", imageSrc: "/equipe-Duda.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Imbituba"},
-  // { name: "Dr. ", role: "Cirurgião | CRMV-SC 0000", imageSrc: "/equipe-1.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Laguna"},
+  { name: "Dr. Douglas Vicentin", role: "Sócio Fundador, Diretor Geral", crmv: "CRMV-SC 5003", imageSrc: "/equipe-Douglas.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Laguna"},
+  { name: "Dra. Ana Lívia Vicentin", role: "Sócia Fundadora, Anestesiologia e Medicina Felina", crmv: "CRMV-SC 04952", imageSrc: "/avatar.png", alt: "Foto do médico(a) Veterinário(a)", unit: "Laguna"},
+  { name: "Dr. Guilherme Pereira", role: "Anestesiologia, Responsável Técnico e Coordenador de equipe", crmv: "CRMV-SC 11054", imageSrc: "/equipe-Gui.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Imbituba"},
+  { name: "Dra. Andressa Spengler", role: "Cirurgia de Tecidos Moles e Oncologia", crmv: "CRMV-SC 14192", imageSrc: "/equipe-Andressa.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Imbituba"},
+  { name: "Dr. Ramon Santiago", role: "Clínica Geral, Medicina Felina", crmv: "CRMV-SC 12456", imageSrc: "/equipe-Ramon.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Imbituba" },
+  { name: "Dra. Marina Noronha", role: "Clínica Geral", crmv: "CRMV-SC 14205", imageSrc: "/equipe-Mari.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Imbituba"},
+  { name: "Dra. Bruna Oliveira", role: "Clínica Geral, Medicina Felina e Cirurgia de Tecidos Moles", crmv: "CRMV-SC 9959", imageSrc: "/equipe-Bruna.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Imbituba"},
+  { name: "Dra. Caroline Tessmer", role: "Ultrassonografia - Diagnóstico por Imagem", crmv: "CRMV-SC 12602", imageSrc: "/equipe-Carol.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Imbituba"}, 
+  { name: "Dra. Letícia Laureano", role: "Clínica Geral", crmv: "CRMV-SC 13580", imageSrc: "/equipe-LeticiaL.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Laguna"}, 
+  { name: "Dra. Bruna Micheleto", role: "Clínica Geral", crmv: "CRMV-SC 15204", imageSrc: "/avatar.png", alt: "Foto do médico(a) Veterinário(a)", unit: "Laguna"},
+  { name: "Dr. Vitor Anacleto", role: "Clínica Geral", crmv: "CRMV-SC 14491", imageSrc: "/equipe-Vitor.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Laguna"},
+  { name: "Dra. Eduarda Sproesser", role: "Clínica Geral", crmv: "CRMV-SC 15381", imageSrc: "/equipe-Duda.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Imbituba"},
+  // { name: "Dr. ", role: "Cirurgião", crmv: "CRMV-SC 0000", imageSrc: "/equipe-1.webp", alt: "Foto do médico(a) Veterinário(a)", unit: "Laguna"},
 ];
 
 const equipeLaguna = equipe.filter(membro => membro.unit === "Laguna");
@@ -50,14 +50,15 @@ export function Equipe() {
               <h3 className="font-poppins text-2xl font-bold text-secondary mb-8 text-center">
                 Unidade Laguna
               </h3>
-              <div className="max-w-6xl mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6 justify-items-center">
+              <div className="max-w-6xl mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 justify-items-center">
                 {equipeLaguna.map((membro) => (
-                  <div key={membro.name} className="flex flex-col items-center text-center space-y-2 max-w-[120px]">
+                  <div key={membro.name} className="flex flex-col items-center text-center space-y-2 max-w-[140px] md:max-w-[200px]">
                     <div className="relative w-32 h-32 rounded-full overflow-hidden shadow-md">
                       <Image src={membro.imageSrc} alt={`Foto de ${membro.name}`} fill className="object-cover" />
                     </div>
                     <h4 className="font-poppins font-semibold text-base leading-tight">{membro.name}</h4>
                     <p className="text-xs text-muted-foreground leading-tight">{membro.role}</p>
+                    <p className="text-xs text-muted-foreground leading-tight">{membro.crmv}</p>
                   </div>
                 ))}
               </div>
@@ -69,14 +70,15 @@ export function Equipe() {
               <h3 className="font-poppins text-2xl font-bold text-secondary mb-8 text-center">
                 Unidade Imbituba
               </h3>
-              <div className="max-w-6xl mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6 justify-items-center">
+              <div className="max-w-6xl mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5  gap-6 justify-items-center">
                 {equipeImbituba.map((membro) => (
-                  <div key={membro.name} className="flex flex-col items-center text-center space-y-2 max-w-[120px]">
+                  <div key={membro.name} className="flex flex-col items-center text-center space-y-2 max-w-[140px] md:max-w-[180px]">
                     <div className="relative w-32 h-32 overflow-hidden shadow-md object-cover rounded-full">
                       <Image src={membro.imageSrc} alt={`Foto de ${membro.name}`} fill className="object-cover rounded-full" />
                     </div>
                     <h4 className="font-poppins font-semibold text-base leading-tight">{membro.name}</h4>
                     <p className="text-xs text-muted-foreground leading-tight">{membro.role}</p>
+                    <p className="text-xs text-muted-foreground leading-tight">{membro.crmv}</p>
                   </div>
                 ))}
               </div>
